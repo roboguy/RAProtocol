7d0ebf34d7ba3365f063962add921235e8ca091f
Sudhanshu Iyer <sudhanshu.iyer@tangomc.com>
2015-03-22 22:46:11 -0500

    Fixed Merge conflict in Shared.java

--- src/main/java/edu/utdallas/aos/p2/Shared.java
+++ src/main/java/edu/utdallas/aos/p2/Shared.java
@@ -22,19 +22,5 @@ public class Shared {
 	public static volatile Object objForLock = new Object();
 	// synchronized(objForLock)
 
-<<<<<<< HEAD
 }
-=======
-	public static volatile boolean isRequestedCS=false;	// if I have requested for CS
-	public static volatile HashSet<String> haveKeys=new HashSet<String>();
-	public static volatile HashSet<String> haveNotKeys=new HashSet<String>();
-	public static volatile Queue<Request> bufferingQueue=new LinkedList<Request>();
-	//Implement Logical CLock Timestamp
-	public static volatile int logicalClockTimeStamp=0;
-	//Request timestamp - kaunsa time par request hui thi.
-	public static volatile int requestTimeStamp=0;
-	public static volatile Object objForLock=new Object();
-	//synchronized(objForLock)
-	
-}
->>>>>>> b36ee8fe1abb22af9c7065cc0fcdac596248f22a
+
